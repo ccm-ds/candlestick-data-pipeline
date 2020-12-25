892a1ab6013f25d1dc42ee6437fd43aa5d0e8a8c
Dan Silva <dan.silva.1194@gmail.com>
2020-12-25 08:32:41 -0500

    local push

--- candlestick_data_pipeline/__init__.py
+++ candlestick_data_pipeline/__init__.py
@@ -1,16 +1,17 @@
 import dask.dataframe as dd
 import os
 
-def read_data(file_path):
+def read_data_by_file_extension(file_path):
   """
   parse file path
   :param file_path: path of
   :return:
   """
-  file_extension_to_read_funtion_lookup = {'.csv': dd.read_csv, '.parquet': dd.read_parquet, '.json': dd.read_json}
+  map_file_extension_to_read_function = {'.csv': dd.read_csv, '.parquet': dd.read_parquet, '.json': dd.read_json}
   name, extension = os.path.splitext(file_path)
   if extension.lower() in file_extension_to_read_funtion_lookup.keys():
-    return file_extension_to_read_funtion_lookup[extension.lower()](file_path)
+    read_function = file_extension_to_read_funtion_lookup[extension.lower()]
+    return read_function(file_path)
   else:
     raise Exception(f"File extention {extension} not recognized")
 
